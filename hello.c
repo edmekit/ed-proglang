2d9c6dda2241aa53abb19afceabd9c98858f9153
Edmarc Mekitpekit <mekitpekit.eggmarc@gmail.com>
2025-10-19 21:51:59 +0800

    parser

--- hello.c
+++ hello.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
+#include "mpc.h"
 static char input[2048];
 
 int main(int argc, char const *argv[])
 {	
+	mpc_parser_t* Number = mpc_new("number");
+	mpc_parser_t* Operator = mpc_new("operator");
+	mpc_parser_t* Expr = mpc_new("expr");
+	mpc_parser_t* Lispy = mpc_new("lispy");
+
+	mpca_lang(MPCA_LANG_DEFAULT,
+		"											\
+		number : /-?[0-9]+/ ; 						\
+		operator : '+' | '-' | '*' | '/' ; 			\
+		expr : <number> | '(' <operator> <expr>+ ')' ;\
+		lispy : /^/ <operator> <expr>+ /$/ ; 		\
+		",
+			Number, Operator, Expr, Lispy);
+
+
 	puts("Lispy Version 0.0.0.0.1");
 	puts("Press Ctrl+C to Exit\n");
 
@@ -11,7 +27,16 @@ int main(int argc, char const *argv[])
 
 		fgets(input,2048,stdin);
 
-		printf("I agree, %s", input );
+		mpc_result_t r;
+		if (mpc_parse("<stdin>", input, Lispy, &r)) {
+			mpc_ast_print(r.output);
+			mpc_ast_delete(r.output);
+		} else {
+			mpc_err_print(r.error);
+			mpc_err_delete(r.error);
+		}
 	}
+
+	mpc_cleanup(4, Number, Operator,Expr, Lispy);
 	return 0;
 }
