4b576190776e5564345a67f196eb9f821fa424a7
Edmarc Mekitpekit <mekitpekit.eggmarc@gmail.com>
2025-11-16 19:47:58 +0800

    eval

--- hello.c
+++ hello.c
@@ -2,6 +2,9 @@
 #include "mpc.h"
 static char input[2048];
 
+long eval(mpc_ast_t* t);
+long eval_op(long x, char* op, long y);
+
 int main(int argc, char const *argv[])
 {	
 	mpc_parser_t* Number = mpc_new("number");
@@ -29,8 +32,10 @@ int main(int argc, char const *argv[])
 
 		mpc_result_t r;
 		if (mpc_parse("<stdin>", input, Lispy, &r)) {
-			mpc_ast_print(r.output);
-			mpc_ast_delete(r.output);
+			mpc_ast_t* a = r.output;
+			long result = eval(a);
+			printf("%li\n", result);
+			mpc_ast_delete(a);
 		} else {
 			mpc_err_print(r.error);
 			mpc_err_delete(r.error);
@@ -38,5 +43,30 @@ int main(int argc, char const *argv[])
 	}
 
 	mpc_cleanup(4, Number, Operator,Expr, Lispy);
+	return 0;
+}
+
+long eval(mpc_ast_t* t){
+	if (strstr(t->tag, "number")) {
+		return atoi(t->contents);
+	}
+
+	char* opera = t->children[1]->contents;
+
+	long x = eval(t->children[2]);
+
+	int i = 3;
+	while(strstr(t->children[i]->tag, "expr")) {
+		x = eval_op(x, opera, eval(t->children[i]));
+		i++;
+	}
+	return x;
+}
+
+long eval_op(long x, char* op, long y) {
+	if (strcmp(op, "+") == 0) {return x + y;}
+	if (strcmp(op, "-") == 0) {return x - y;}
+	if (strcmp(op, "*") == 0) {return x * y;}
+	if (strcmp(op, "/") == 0) {return x / y;}
 	return 0;
 }
